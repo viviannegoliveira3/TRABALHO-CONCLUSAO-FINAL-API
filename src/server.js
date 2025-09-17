b2ae658bd77a3245f93f7519ab75487f390c4798
Vivianne Oliveira <vivianne.oliveira@gmail.com>
2025-09-17 17:00:12 -0300

    Adicionando arquivos

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
