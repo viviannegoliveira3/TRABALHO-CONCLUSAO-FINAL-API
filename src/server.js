4075603dc1169d70417cbfa68370df0257093096
Vivianne Oliveira <vivianne.oliveira@gmail.com>
2025-09-17 19:28:01 -0300

    Adicionando testes a pipeline

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
