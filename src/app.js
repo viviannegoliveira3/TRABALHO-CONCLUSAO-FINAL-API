694df98b4dd056c673ef8af067941aa6b41336c0
Vivianne Oliveira <vivianne.oliveira@gmail.com>
2025-09-18 16:08:18 -0300

    Habilitando a pipeline no Github Actions

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 
+const userRoutes = require('./routes/userRoutes');
+const checkoutRoutes = require('./routes/checkoutRoutes');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -25,6 +28,8 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// TODO: Adicionar suas outras rotas aqui (ex: app.use('/api/users', userRoutes);)
+// Registra as rotas da aplicação
+app.use('/api/users', userRoutes);
+app.use('/api/checkout', checkoutRoutes);
 
 module.exports = app;
