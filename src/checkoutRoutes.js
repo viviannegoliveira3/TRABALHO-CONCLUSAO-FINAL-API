fe744976347d4ff2b3dd43d8b018f089b4a70140
Vivianne Oliveira <vivianne.oliveira@gmail.com>
2025-09-18 17:17:28 -0300

    Habilitando a pipeline no Github Actions

--- src/checkoutRoutes.js
+++ src/checkoutRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
 
-// Mock de um controller e middleware, substitua pela sua lógica real
-const authMiddleware = (req, res, next) => next(); // Middleware de autenticação mock
 const checkoutController = {
   process: (req, res) => {
     res.status(200).json({ message: 'Checkout successful' });
